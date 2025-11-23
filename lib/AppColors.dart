6310780425bfa327332eee7edb3cc10cdd7845ab
padilla08flow-glitch <padilla08flow@gmail.com>
2025-11-22 19:21:32 -0600

    Add AppColors class with color constants

---
+++ lib/AppColors.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: type=lint
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color mexicanPink = Color(0xffEE6983);
+  static const Color softPink = Color(0xffE06B80);
+  static const Color lightBackground = Color(0xffFEF2F2);
+  static const Color darkAccent = Color.fromARGB(255, 74, 39, 56);
+  static const Color blueBonito = Color(0xff6E8CFB);
+  static const Color ErrorRojo = Color(0xffff0000);
+  
+}
